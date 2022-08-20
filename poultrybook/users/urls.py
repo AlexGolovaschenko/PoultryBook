d1c4444d216c57db41bc2f47a6bdbe1a7ca371bc
Golovaschenko Alex <sa.golovaschenko@gmail.com>
2022-08-20 10:14:27 +0300

    fixed link

--- poultrybook/users/urls.py
+++ poultrybook/users/urls.py
@@ -5,5 +5,5 @@ app_name = 'users'
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='users/logout.html', next_page='base:home'), name='logout'), 
+    path('logout/', LogoutView.as_view(template_name='users/logout.html', next_page='logbook:workspace'), name='logout'), 
 ]
